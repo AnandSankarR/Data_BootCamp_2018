1cedf2cada7e979ac3eeebda322b96efb1b259bd
anandsankar <anandsankar.r@gmail.com>
2018-02-21 11:56:26 -0800

    FirstCommit

---
+++ learn_python/assignment3/variable.py
@@ -0,0 +1,8 @@
+title="FrankFurter"
+years=80
+trueOrfalse = False
+print("Ahmed is a professional" + title)
+print("He has been coding for" + str(years))
+print("pretty much all of that was "+ str(trueorfalse))
+
+
---
+++ learn_python/assignment4/variableworld.py
@@ -0,0 +1,14 @@
+name ="Anand"
+country="India"
+age=24
+hourlywage=10
+dailywage= hourlywage * 8
+satisfied = True
+print("Hello" + name+"!")
+print(name+"'s"+" country is"+ country)
+print(name+"'s"+"Age is" +str(age))
+print(name+"'s"+ "daily wage is" + str(dailywage))
+print("are you satisfied?"+str(satisfied))
+
+
+
